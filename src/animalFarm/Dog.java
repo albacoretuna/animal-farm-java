e1ed8e86e0bba8f7bbe91da2dfba67272d8b8924
gikfi <ommmmid@gmail.com>
2014-10-27 20:39:54 +0200

    Added the name changer functionality

--- src/animalFarm/Dog.java
+++ src/animalFarm/Dog.java
@@ -5,25 +5,33 @@ import javax.swing.JOptionPane;
 public class Dog extends Mamal{
 	
 	private int age;
-	
+	private String name;
 	public Dog() {
 	}
 	
-	public Dog(int age, int legsAndHandsTotal, String isExtincted, String isDomestic, String isVegeterian, String latinName) {
+	public Dog(String name, int age, int legsAndHandsTotal, String isExtincted, String isDomestic, String isVegeterian, String latinName) {
 		super(legsAndHandsTotal, isExtincted, isDomestic, isVegeterian, latinName);
-		this.age = age; 
+		this.name = name; 
+		this.age = age;
 	}
 	
-	public int getAge() {
-		return age;
+
+	public String getName() {
+		return name;
 	}
+	
+	public void setName (String name) {
+		this.name = name;
+	}
+	
 	
     @Override
     public void introduce() {
     	
     	
-        String message = "Here I introduce a cute dog: \n "
-        		  + "Name: " + latinName
+        String message = "      Here I introduce a cute dog: \n     "
+        		  + "Scienefic Name: " + latinName
+        		  + "\n Name : " + name
                   + "\n Is extinct: "   + isExtincted 
                   + "\n Age: " + age 
                   + "\n Number of Legs and Hands: " + legsAndHandsTotal
--- src/animalFarm/MyAnimalFarm.java
+++ src/animalFarm/MyAnimalFarm.java
@@ -7,13 +7,26 @@ import javax.swing.JOptionPane;
 public class MyAnimalFarm {
     public static void main(String[ ] a) {
     	
-    	String res = JOptionPane.showInputDialog("Enter value:");
     	Animal [] my = new Animal[3];
-    	my [0]= new Dog(7, 4, "No", "Yes", "No", "Cavalier King Charles Spaniel");
+    	my [0]= new Dog("No Name Yet", 4 , 4 , "No", "Yes", "No", "Cavalier King Charles Spaniel");
     	my [1]= new Bluejay(14, 4, "No", "No", "No", "Bluejay Sir Alex Fergusen");
     	my [2]= new Human(98, 4, "No", "Yes", "No", "Frantz Kafka");    	
     	for(int i = 0; i < 3; i++)
     		my[i].introduce();
+    	
+        String nameForDog=JOptionPane.showInputDialog("You can now Name the Dog. Enter its name below: ");
+        ((Dog) my[0]).setName(nameForDog);
+        
+        // TODO do the same thing for BlueJay and Human 
+        String nameForBird=JOptionPane.showInputDialog("You can now Name the Bird. Enter its name below: ");
+        ((Dog) my[1]).setName(nameForDog);
+        String nameForMan=JOptionPane.showInputDialog("You can now Name the Guy. Enter his name below: ");
+        ((Dog) my[2]).setName(nameForDog);
+       // String newNameForDog =  ((Dog) my[0]).getName();
+        //JOptionPane.showMessageDialog(null,"New Name for The Dog is: "+ newNameForDog);
+		my[0].introduce();
+
+ 
     	
     }
 
